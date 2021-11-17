f59fc262e4ce9f4e2084a6c184f38f32830790fe
rocfatcat <rocfatcat@gmail.com>
2021-11-17 18:53:59 +0800

    Add DBContext Object
    Change-Id: I4c51ad74cb8b68ed38018590ec4df40bc766e60e

---
+++ Model/EFCore6DBContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Dotnet6EFCore.Model
+{
+    public partial class EFCore6DBContext : DbContext
+    {
+        public EFCore6DBContext()
+        {
+        }
+
+        public EFCore6DBContext(DbContextOptions<EFCore6DBContext> options)
+            : base(options)
+        {
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDB;Database=Dotnet_EFCore_6;Trusted_Connection=True;MultipleActiveResultSets=true");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}

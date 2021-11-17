5aa40fe48bf422444626fce656da0e6f0cb62f69
rocfatcat <rocfatcat@gmail.com>
2021-11-17 18:35:22 +0800

    Add MSSQL LocalDB Create Powershell Script File
    Change-Id: Ie36364c52e9b092d586b9f9a31d7c0bbc7cadd4d

---
+++ create_db.ps1
@@ -0,0 +1,9 @@
+$name = Read-Host -Prompt 'Input your db name default:Dotnet_EFCore_6'
+if ([string]::IsNullOrWhiteSpace($name))
+{
+    $name = "Dotnet_EFCore_6"
+}
+$dbname = "" + $PWD + "\$name.mdf" #// 資料庫的名稱加位置
+$query = "Create Database $name on primary (Name='$name',Filename='" + $dbname + "');" #// 建立資料庫的指令
+echo $query
+sqlcmd -S '(LocalDb)\MSSQLLocalDB' -q $query #// 使用sqlcmd來創建資料庫
---
+++ drop_db.ps1
@@ -0,0 +1,7 @@
+$name = Read-Host -Prompt 'Input your db name default:Dotnet_EFCore_6'
+if ([string]::IsNullOrWhiteSpace($name)) {
+    $name = "Dotnet_EFCore_6"
+}
+$query = "drop Database $name" #// 建立資料庫的指令
+echo $query
+sqlcmd -S '(LocalDb)\MSSQLLocalDB' -q $query #/

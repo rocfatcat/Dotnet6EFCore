9c41eb740327dbe3e9388f8f3e654cf68ab58ceb
rocfatcat <rocfatcat@gmail.com>
2021-11-24 10:34:31 +0800

    Add Model Migrations File
    Change-Id: I9b5c8ee329581bea8134c3c4971f47e5b5bb4b3c

---
+++ Migrations/20211124023354_InitDB.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Dotnet6EFCore.Migrations
+{
+    public partial class InitDB : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Meetings",
+                columns: table => new
+                {
+                    MeetingId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    MeetingName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StartDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    ENDDate = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Meetings", x => x.MeetingId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Phone = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    OnboardDate = table.Column<DateTime>(type: "datetime2", nullable: true),
+                    NowMeetingMeetingId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                    table.ForeignKey(
+                        name: "FK_Users_Meetings_NowMeetingMeetingId",
+                        column: x => x.NowMeetingMeetingId,
+                        principalTable: "Meetings",
+                        principalColumn: "MeetingId");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_NowMeetingMeetingId",
+                table: "Users",
+                column: "NowMeetingMeetingId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Meetings");
+        }
+    }
+}
